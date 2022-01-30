1f20a1b58a33b50b371fac1dbb9cccc95d9f6784
AllePansan <alexpjunior@terra.com.br>
2022-01-30 20:39:25 -0300

    added meta tags for SEO

--- pages/index.jsx
+++ pages/index.jsx
@@ -2,19 +2,26 @@
 import Body from "./component/Body"
 import Header from "./component/Header"
 import React, { useState, useEffect } from 'react';
+import Head from 'next/head'
 
 export default function Index() {
   const [cart, setCart] = useState([]);
   const CallBackCart = (data) => {
-    //console.log(data)
-    //setCart(data)
     setCart(data)
-    //console.log(data)
 }
 
   
   return (
     <>
+      <Head>
+        <title>KaBum - Simplificada</title>
+        <meta name="viewport" content="initial-scale=1.0, width=device-width" />
+        <meta name="description" content="Encontre as melhores ofertas em Tecnologia e Games aqui no KaBum." />
+        <meta name="language" content="pt-br" />
+        <meta name="keywords" content="kabum, loja, kabum loja, tecnologia, ofertas, games, loja online, compras, hardware, kabum.com.br, ofertas de hardware, promoçoes, kabum é boa, kabum é seguro, kabum é confiavel"/>
+        <meta name="author" content="KaBUm! | Maior E-comerce de técnologia da américa latina"/>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
       <Header cart={cart}>
         
       </Header>
